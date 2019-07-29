f65591e9921e676e9fae87a25664ef5362d51997
Dario Cortes <laf.dariocortes@gmail.com>
2019-07-28 23:37:50 -0500

    nuevo ejercicio con 5 perritos

--- perros.js
+++ perros.js
@@ -1,25 +1,30 @@
 // 1. VARIABLES
-const perritosArea = document.getElementById("perritos")
-const perritosBreedArea = document.getElementById("perritosBreed")
-const botonPerritos = document.getElementById("dameperritos")​
-    // 2. FUNCIONES
+const perritos = document.getElementById("perritos")
+
+//const perritosBreedArea = document.getElementById("perritosBreed")
+const botonPerritos = document.getElementById("dameperritos")
+
+// 2. FUNCIONES
 const extraerPerritos = () => {
-        fetch("https://dog.ceo/api/breeds/image/random/3").then((response) => {
-            return response.json()
-        }).then((data) => {
-            console.log(data)
-            perritosArea.innerHTML = `
+    fetch("https://dog.ceo/api/breeds/image/random/5").then((response) => {
+        return response.json()
+    }).then((data) => {
+        console.log(data)
+        perritos.innerHTML = `
         <p>Primer Perro</p>
         <img src="${data.message[0]}" />
         <p>Segundo Perro</p>
         <img src="${data.message[1]}" />
         <p>Tercer Perro</p>
         <img src="${data.message[2]}" />
+        <p>Cuarto Perrito</p>
+        <img src="${data.message[3]}" />
+        <p>Quinto Perrito</p>
+        <img src="${data.message[4]}" />
     `
-        })
-    }​
-    // 3. EVENTOS
-    ​
+    })
+}
+
 botonPerritos.addEventListener("click", () => {
     extraerPerritos()
 })
